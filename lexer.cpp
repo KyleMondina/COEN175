4ed3aed3833801d110d75a2da61c544e88d3a683
KyleMondina <kylemondina@gmail.com>
2022-01-08 17:50:40 -0800

    added handlers for white space and numbers, not yet tested

--- lexer.cpp
+++ lexer.cpp
@@ -4,6 +4,11 @@
 #include <unordered_set>
 
 //function to handle simple C keywords
+
+enum {
+	NUMBER,STRING,CHARACTER,IDENTIFIER,KEYWORD,OPERATOR,DONE,
+};
+
 static int isKeyword(std::string s){
 
 	std::unordered_set<std::string> keywords = {
@@ -64,6 +69,89 @@ static int isSingleOperator(char c){
 }
 
 static int isDoubleOperator(std::string s){
+	std::unordered_set<std::string> doubleOperators = {
+		"||",
+		"&&",
+		"==",
+		"!=",
+		"<=",
+		">=",
+		"++",
+		"--",
+		"->"
+	};
+
+	if (doubleOperators.find(s)!=doubleOperators.end()){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+static int isPotentialDoubleOperator(char c){
+	std::unordered_set<char> potentialDouble = {
+		'<',
+		'>',
+		'!',
+		'&',
+		'=',
+		'+',
+		'-',
+		'|'
+
+	};
+	if (potentialDouble.find(c) != potentialDouble.end()){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+static int lexan(std::string& lexbuff){
+
+	char input = std::cin.peek();
+
+	//for dealing with operators
+	char xbuff;
+	char ybuff;
+	std::string opbuff;
+
+	while (!std::cin.eof()){
+
+		lexbuff.clear();
+
+		//1.) White Space
+		if(isWhiteSpace(input)){
+			do{
+				std::cin.get();
+				input = std::cin.peek();
+			}while(isWhiteSpace(input));
+		}
+
+		//2.) Integers
+		if(std::isdigit(input)){
+			do{
+				lexbuff += input;
+				std::cin.get();
+				input = std::cin.peek();
+			}
+			while(isdigit(input));
+			return NUMBER;
+		}
+
+		
+	}
+	return DONE;
+}
+
+
+
+
+
+
+
+
+
 
 
 
@@ -72,12 +160,6 @@ static int isDoubleOperator(std::string s){
 
 int main(){
 
-	std::string cookie = "while";
 
-	int check = isKeyword(cookie);
-	if (check ==1){
-		std::cout << "iskeyword";
-	}else{
-		std::cout << "noope";
-	}
 }
+
